c6c4545f66b0989728041d47ede7e885175e4c41
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2023-10-12 20:12:12 +0300

    #26 add macros to handlers

--- src/ChaiVM/interpreter/executor.cpp
+++ src/ChaiVM/interpreter/executor.cpp
@@ -2,13 +2,14 @@
 
 namespace chai::interpreter {
 
+#define DO_NEXT_INS()                                                          \
+    Instruction newIns =                                                       \
+        decoder::parse(codeManager_->getBytecode(regFile_.pc()));              \
+    (this->*handlerArr[newIns.operation])(newIns);
+
 Executor::Executor(CodeManager *manager)
     : codeManager_(manager), regFile_(codeManager_->startPC()) {}
-void Executor::run() {
-    const Instruction first =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[first.operation])(first);
-}
+void Executor::run() { DO_NEXT_INS() }
 const RegisterFile &Executor::getState() const & { return regFile_; }
 void Executor::inv(Instruction ins) {
     throw InvalidInstruction("Invalid operation at pc: " +
@@ -16,9 +17,7 @@ void Executor::inv(Instruction ins) {
 }
 void Executor::nop(Instruction ins) {
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::ret(Instruction ins) {
     advancePc();
@@ -27,166 +26,124 @@ void Executor::ret(Instruction ins) {
 void Executor::mov(Instruction ins) {
     regFile_[ins.r2] = regFile_[ins.r1];
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::ldia(Instruction ins) {
     regFile_.acc() = ins.immidiate;
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::ldra(Instruction ins) {
     regFile_.acc() = regFile_[ins.r1];
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::star(Instruction ins) {
     regFile_[ins.r1] = regFile_.acc();
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::add(Instruction ins) {
     regFile_.acc() += regFile_[ins.r1];
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::addi(Instruction ins) {
     regFile_.acc() += ins.immidiate;
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::sub(Instruction ins) {
     regFile_.acc() = regFile_[ins.r1] - regFile_.acc();
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::subi(Instruction ins) {
     regFile_.acc() = ins.immidiate - regFile_.acc();
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::mul(Instruction ins) {
     regFile_.acc() *= regFile_[ins.r1];
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::muli(Instruction ins) {
     regFile_.acc() *= ins.immidiate;
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::div(Instruction ins) {
     regFile_.acc() = regFile_[ins.r1] / regFile_.acc();
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::divi(Instruction ins) {
     regFile_.acc() = ins.immidiate / regFile_.acc();
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::ldiaf(Instruction ins) {
     double immd = static_cast<double>(std::bit_cast<float>(ins.immidiate));
     regFile_.acc() = std::bit_cast<chsize_t>(immd);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::addf(Instruction ins) {
     double sum = std::bit_cast<double>(regFile_[ins.r1]) +
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(sum);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::addif(Instruction ins) {
     double sum = static_cast<double>(std::bit_cast<float>(ins.immidiate)) +
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(sum);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::subf(Instruction ins) {
     double dif = std::bit_cast<double>(regFile_[ins.r1]) -
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(dif);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::subif(Instruction ins) {
     double dif = static_cast<double>(std::bit_cast<float>(ins.immidiate)) -
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(dif);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::mulf(Instruction ins) {
     double sum = std::bit_cast<double>(regFile_[ins.r1]) *
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(sum);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::mulif(Instruction ins) {
     double dif = static_cast<double>(std::bit_cast<float>(ins.immidiate)) *
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(dif);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::divf(Instruction ins) {
     double sum = std::bit_cast<double>(regFile_[ins.r1]) /
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(sum);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 void Executor::divif(Instruction ins) {
     double dif = static_cast<double>(std::bit_cast<float>(ins.immidiate)) /
                  std::bit_cast<double>(regFile_.acc());
     regFile_.acc() = std::bit_cast<chsize_t>(dif);
     advancePc();
-    Instruction newIns =
-        decoder::parse(codeManager_->getBytecode(regFile_.pc()));
-    (this->*handlerArr[newIns.operation])(newIns);
+    DO_NEXT_INS()
 }
 
 InvalidInstruction::InvalidInstruction(const char *msg) : runtime_error(msg) {}
