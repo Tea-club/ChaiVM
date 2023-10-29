6c1dacdf1a6893017423f12cff6e7fded4862fd7
levBagryansky <28lev11@gmail.com>
2023-10-29 23:15:59 +0300

    no bitcast

--- src/ChaiVM/interpreter/executor.cpp
+++ src/ChaiVM/interpreter/executor.cpp
@@ -48,58 +48,46 @@ void Executor::star(Instruction ins) {
     DO_NEXT_INS()
 }
 void Executor::add(Instruction ins) {
-    regFile_.acc() =
-        std::bit_cast<chsize_t>(std::bit_cast<int64_t>(regFile_.acc()) +
-                                std::bit_cast<int64_t>(regFile_[ins.r1]));
+    regFile_.acc() += regFile_[ins.r1];
     advancePc();
     DO_NEXT_INS()
 }
 void Executor::addi(Instruction ins) {
-    regFile_.acc() = std::bit_cast<chsize_t>(
-        std::bit_cast<int64_t>(regFile_.acc()) +
-        std::bit_cast<int64_t>(codeManager_->getCnst(ins.immidiate)));
+    regFile_.acc() += codeManager_->getCnst(ins.immidiate);
     advancePc();
     DO_NEXT_INS()
 }
 void Executor::sub(Instruction ins) {
-    regFile_.acc() =
-        std::bit_cast<chsize_t>(std::bit_cast<int64_t>(regFile_.acc()) -
-                                std::bit_cast<int64_t>(regFile_[ins.r1]));
+    regFile_.acc() -= regFile_[ins.r1];
     advancePc();
     DO_NEXT_INS()
 }
 void Executor::subi(Instruction ins) {
-    regFile_.acc() = std::bit_cast<chsize_t>(
-        std::bit_cast<int64_t>(regFile_.acc()) -
-        std::bit_cast<int64_t>(codeManager_->getCnst(ins.immidiate)));
+    regFile_.acc() -= codeManager_->getCnst(ins.immidiate);
     advancePc();
     DO_NEXT_INS()
 }
 void Executor::mul(Instruction ins) {
-    regFile_.acc() =
-        std::bit_cast<chsize_t>(std::bit_cast<int64_t>(regFile_.acc()) *
-                                std::bit_cast<int64_t>(regFile_[ins.r1]));
+    regFile_.acc() *= regFile_[ins.r1];
     advancePc();
     DO_NEXT_INS()
 }
 void Executor::muli(Instruction ins) {
-    regFile_.acc() = std::bit_cast<chsize_t>(
-        std::bit_cast<int64_t>(regFile_.acc()) *
-        std::bit_cast<int64_t>(codeManager_->getCnst(ins.immidiate)));
+    regFile_.acc() *= codeManager_->getCnst(ins.immidiate);
     advancePc();
     DO_NEXT_INS()
 }
 void Executor::div(Instruction ins) {
     regFile_.acc() =
-        std::bit_cast<chsize_t>(std::bit_cast<int64_t>(regFile_.acc()) /
-                                std::bit_cast<int64_t>(regFile_[ins.r1]));
+        static_cast<chsize_t>(std::bit_cast<int64_t>(regFile_.acc()) /
+                                static_cast<int64_t>(regFile_[ins.r1]));
     advancePc();
     DO_NEXT_INS()
 }
 void Executor::divi(Instruction ins) {
     regFile_.acc() = std::bit_cast<chsize_t>(
-        std::bit_cast<int64_t>(regFile_.acc()) /
-        std::bit_cast<int64_t>(codeManager_->getCnst(ins.immidiate)));
+        static_cast<int64_t>(regFile_.acc()) /
+        static_cast<int64_t>(codeManager_->getCnst(ins.immidiate)));
     advancePc();
     DO_NEXT_INS()
 }
