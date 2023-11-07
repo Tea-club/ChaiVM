dcfc9e821874a32b20a6d57ee739422176aca6a7
levBagryansky <28lev11@gmail.com>
2023-11-07 13:46:43 +0300

    transferred CodeManager::loadFunction it to cpp

--- src/ChaiVM/interpreter/code-manager.cpp
+++ src/ChaiVM/interpreter/code-manager.cpp
@@ -75,6 +75,44 @@ void CodeManager::load(const std::filesystem::path &path) {
     }
 }
 
+void CodeManager::loadFunction(std::istream &istream) {
+    uint16_t access_flags = 0;
+    istream.read(reinterpret_cast<char *>(&access_flags), sizeof access_flags);
+    Immidiate const_ref = 0;
+    istream.read(reinterpret_cast<char *>(&const_ref), sizeof const_ref);
+    Immidiate atts_count = 0;
+    istream.read(reinterpret_cast<char *>(&atts_count), sizeof atts_count);
+    // only "Code" attribute is supported
+    assert(atts_count == 1);
+
+    // read code attribute
+    Immidiate att_name_index = 0;
+    istream.read(reinterpret_cast<char *>(&att_name_index),
+                 sizeof att_name_index);
+    uint32_t att_len = 0;
+    istream.read(reinterpret_cast<char *>(&att_len), sizeof att_len);
+    uint8_t max_regs = 0;
+    istream.read(reinterpret_cast<char *>(&max_regs), sizeof max_regs);
+    uint8_t nargs = 0;
+    istream.read(reinterpret_cast<char *>(&nargs), sizeof nargs);
+    // size of instructions in function in bytes
+    uint32_t code_len = 0;
+    istream.read(reinterpret_cast<char *>(&code_len), sizeof code_len);
+
+    assert(code_len % sizeof(bytecode_t) == 0);
+    bytecode_t bytecode = 0;
+    const size_t next = funcs_.size();
+    funcs_.push_back(Function{
+        .num_regs = max_regs,
+        .num_args = nargs,
+    });
+    for (uint i = 0; i < code_len / sizeof(bytecode_t); ++i) {
+        istream.read(reinterpret_cast<char *>(&bytecode), sizeof(bytecode_t));
+        funcs_[next].code.push_back(bytecode);
+    }
+    dispatch_[const_ref] = next;
+}
+
 chsize_t CodeManager::getCnst(Immidiate id) {
     assert(id < constantPool_.size());
     return constantPool_[id];
